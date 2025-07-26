9ae20899f21236791478baf17a814b02f5df8ad4
ritik kumar <148605032+ritik-kumar123@users.noreply.github.com>
2025-07-26 11:41:04 +0530

    Update error.middlware.js

--- server/middlewares/error.middlware.js
+++ server/middlewares/error.middlware.js
@@ -1,9 +1,10 @@
 export const errorMiddleware = (err, req, res, next) => {
-    err.statusCode = err.statusCode || 500;
-    err.message = err.message || "Internal Server Error";
+  console.log("🔥 ERROR:", err); 
+  err.statusCode = err.statusCode || 500;
+  err.message = err.message || "Internal Server Error";
 
-    res.status(err.statusCode).json({
-        success: false,
-        errMessage: err.message,
-    });
+  res.status(err.statusCode).json({
+    success: false,
+    errMessage: err.message,
+  });
 };
